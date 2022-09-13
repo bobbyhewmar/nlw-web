16935c71fa28101cffdca57b7c3bdd774afd6210
thzinnn <thiagom@speediomail.com>
2022-09-12 23:25:17 -0300

    add modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,28 @@ import { useState } from "react";
 import { Carousel } from "./components/Carousel";
 import { DuoPublicher } from "./components/DuoPublicher";
 import { Logo } from "./components/Logo";
+import { Modal } from "./components/Modal";
 import { Title } from "./components/Title";
 import { res } from "./utils/data"
 
 function App() {
   const [data] = useState(res)
+  const [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
 
   return <>
     <Logo />
     <Title />
     <Carousel data={data} />
-    <DuoPublicher />
+    <DuoPublicher openModal={openModal} />
+    <Modal closeModal={closeModal} isOpen={isOpen} />
   </>
 }
 
--- src/components/DuoPublicher.tsx
+++ src/components/DuoPublicher.tsx
@@ -1,6 +1,10 @@
 import {IconSearch} from "./Icons/IconSearch"
 
-export function DuoPublicher () {
+type Props = {
+  openModal: () => void;
+}
+
+export function DuoPublicher ({ openModal }: Props) {
     return (
         <div className="w-full my-8 rounded-lg bg-gradient-to-r from-[#9572FC] via-[#43E7AD] to-[#E1D55D]">
             <div className="flex flex-col md:flex-row w-full py-6 px-8 bg-[#2A2634] justify-between items-center text-white rounded-md text-base mt-1">
@@ -8,7 +12,7 @@ export function DuoPublicher () {
                     <span className="font-black text-2xl">Não encontrou seu duo?</span>
                     <span className="text-zinc-400">Publique um anúncio para encontrar novos players!</span>
                 </div>
-                <button className="mt-4 md:mt-0 w-full md:w-auto bg-violet-500 flex py-3 px-4 rounded-md font-medium gap-3">
+                <button className="mt-4 md:mt-0 w-full md:w-auto bg-violet-500 flex py-3 px-4 rounded-md font-medium gap-3" onClick={() => openModal()}>
                     <IconSearch />
                     Publicar anúncio
                 </button>
